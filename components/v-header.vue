da1f0b9a4f802e64f774d735d2feb5b20e2526b7
Gentleman <gentleman-2021@mail.ru>
2021-06-20 02:50:58 +0300

    Minor changes

--- components/v-header.vue
+++ components/v-header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
-    <svg @click="showSideBar" v-show="this.propShowSidebar !== true" class="header__open-sidebar-hamburger open-sidebar-hamburger" width="30px" height="30px" id="Outline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>183 burger menu</title><rect y="11" width="24" height="2" rx="1"/><rect y="4" width="24" height="2" rx="1"/><rect y="18" width="24" height="2" rx="1"/></svg>
-    <svg @click="hideSideBar" v-show="this.propShowSidebar !== false" class="header__hide-sidebar-arrow hide-sidebar-arrow" width="40px" height="40px"
+    <svg @click="showSideBar" v-show="propShowSidebar !== true" class="header__open-sidebar-hamburger open-sidebar-hamburger" width="30px" height="30px" id="Outline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect y="11" width="24" height="2" rx="1"/><rect y="4" width="24" height="2" rx="1"/><rect y="18" width="24" height="2" rx="1"/></svg>
+    <svg @click="hideSideBar" v-show="propShowSidebar !== false" class="header__hide-sidebar-arrow hide-sidebar-arrow" width="40px" height="40px"
          id="Outline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path
         d="M10.88,17.715a1,1,0,0,0,0-1.415L7.588,13.007,18,13a1,1,0,0,0,0-2l-10.414.007L10.88,7.715A1,1,0,0,0,9.466,6.3L5.88,9.886a3,3,0,0,0,0,4.243l3.586,3.586A1,1,0,0,0,10.88,17.715Z"/>

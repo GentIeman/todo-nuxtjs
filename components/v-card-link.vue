5d72324a527bdb6a648b6f21298eb7d97419d21c
Gentleman <gentleman-2021@mail.ru>
2021-06-22 23:56:58 +0300

    Fixing styles and changing the architecture of the component

--- components/v-card-link.vue
+++ components/v-card-link.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="card">
-    <div class="card__content">
-      <div class="card__item card__content-title">
-        <label>Задача</label>
-        <p class="card__title title">{{ data.title }}</p>
+    <div class="card__inner">
+      <div class="card__content">
+        <div class="card__item card__content-title">
+          <label>Задача</label>
+          <p class="card__title title">{{ data.title }}</p>
+        </div>
+        <div class="card__item card__content-date">
+          <label>Время выполения</label>
+          <p class="card__date date">{{ data.date }} {{ formatHours(data.date) }}</p>
+        </div>
       </div>
-      <div class="card__item card__content-date">
-        <label>Время выполения</label>
-        <p class="card__date date">{{ data.date }} {{ formatHours(data.date) }}</p>
-      </div>
-      <div class="card__item card__icon-link" @mouseover="hoverIcon = 'link'"  @mouseleave="hoverIcon = 'unlink'">
+      <div class="card__item card__icon-link" @mouseover="hoverIcon = 'link'" @mouseleave="hoverIcon = 'unlink'">
         <img class="icon-link" :src="'/icons/' + hoverIcon  +  '.svg'" alt="icon" width="25px" height="25px">
       </div>
     </div>
@@ -37,25 +39,35 @@ export default {
 .card {
   display flex
   justify-content space-between
-  align-items center
   width 100%
   height 60px
   border-bottom solid 1px #ccc
   transition background-color 1s ease
   cursor pointer
 
-  &__content {
+  &__inner {
     display flex
     justify-content space-between
     align-items flex-end
     position relative
     width 100%
-    height 100%
     bottom 6px
 
-    label {
-      font normal 0.8em sans-serif
-      color #444953
+    .card__content {
+      display flex
+      align-items flex-end
+      position relative
+      width 100%
+      height 100%
+
+      .card__content-title {
+        min-width 300px
+      }
+
+      label {
+        font normal 0.8em sans-serif
+        color #444953
+      }
     }
   }
 

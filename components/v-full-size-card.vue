a1c04bca27ce9f3e0ef53289b104981ba2e41f7a
GentIeman <76070836+GentIeman@users.noreply.github.com>
2021-07-05 22:50:44 +0300

    the linking card has been moved to a separate component v-linked-card

--- components/v-full-size-card.vue
+++ components/v-full-size-card.vue
@@ -21,33 +21,21 @@
           карточкой</a>
       </div>
     </div>
-    <div class="linked" v-if="data.linked">
-      <div class="linked__bunch">
-        <img :src="'/icons/' + linkIcon + '.svg'" alt="icon" width="30px" height="30px" @mousemove="linkIcon = 'cancel'" @mouseleave="linkIcon = 'bunch'" @click="dropLink">
-      </div>
-      <div class="linked-card">
-        <vs-checkbox dark class="linked-card__checkbox"
-                     @change="changeTask({id: data.id, toEdit: 'status', value: !data.status})"
-                     :checked-force="data.status"></vs-checkbox>
-        <div class="linked-card__main-data">
-          <header class="linked-card__item linked-card__header">
-            <h2 class="title">{{ getTaskById(data.linked).title }}</h2>
-          </header>
-          <div class="linked-card__item linked-card__hour">
-            <p class="date">{{ getTaskById(data.linked).date }} {{ formatHours(getTaskById(data.linked).date) }}</p>
-          </div>
-        </div>
-      </div>
+    <div class="linked__bunch" v-if="data.linked">
+      <img class="bunch" :src="'/icons/' + linkIcon + '.svg'" alt="icon" width="30px" height="30px" @mousemove="linkIcon = 'cancel'" @mouseleave="linkIcon = 'bunch'" @click="dropLink">
     </div>
-    <v-link v-if="showModalWindowLink" @close="showModalWindowLink = false" :currentId="data.id"/>
+    <v-linked-card :data="data"/>
+    <v-link-list v-if="showModalWindowLink" @close="showModalWindowLink = false" :currentId="data.id"/>
   </section>
 </template>
 
 <script>
 import {mapActions, mapGetters} from "vuex";
 import formatHours from '@/mixins/format-hours.js';
+import VLinkedCard from "./v-linked-card";
 
 export default {
+  components: {VLinkedCard},
   data: () => ({
     showFullSizeCard: false,
     isSlideUp: false,
@@ -126,6 +114,7 @@ export default {
       background-color #272732
       border-radius 10px
       box-shadow rgba(0, 0, 0, 0.7) 0px 3px 8px 0px
+      cursor default
 
       &__checkbox {
         position relative
@@ -237,58 +226,12 @@ export default {
     }
   }
 
-  .linked {
+  .linked__bunch {
+    display flex
+    justify-content center
     position relative
-    top 50px
-    width 800px
-    height auto
-
-    &__bunch {
-      display flex
-      justify-content center
-      width 100%
-      height 50px
-    }
-
-    .linked-card {
-      display flex
-      align-items center
-      position relative
-      min-width 800px
-      background-color #272732
-      height 80px
-      border-radius 10px
-
-      &__main-data {
-        display flex
-        justify-content center
-        align-items flex-start
-        flex-direction column
-        flex-wrap wrap
-        position relative
-        left 30px
-        width auto
-        height 100%
-        padding 0 15px
-
-        &:after {
-          content '';
-          position absolute
-          top 50%
-          left 100%
-          transform translate(-50%, -50%) rotate(90deg)
-          width 44px
-          height 1px
-          background-color #64B5A2
-        }
-      }
-
-      &__checkbox {
-        position relative
-        top 0
-        left 25px
-      }
-    }
+    top 30px
+    width auto
   }
 
   .title {
